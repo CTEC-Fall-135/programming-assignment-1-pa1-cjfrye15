1c0655fc933818faf34c3a4ad52576f4136c1b75
cjfrye15 <166461518+cjfrye15@users.noreply.github.com>
2024-09-30 14:55:54 -0700

    Initial
    Initial

---
+++ Module1/Module1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Caleb Frye
+ * 9/30/24
+ * PA1 */
+
+namespace Module1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My favorite thing about winter is snowboarding");
+            Console.WriteLine("My favorite color is purple");
+            Console.WriteLine("I like pizza");
+            Console.WriteLine("My favorite movie is the animated SpiderMan ");
+        }
+    }
+}
